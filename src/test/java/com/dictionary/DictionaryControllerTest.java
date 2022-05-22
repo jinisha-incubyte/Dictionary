7a3a8e3ca5ecfcf2c6a7c93475f123fe4d3d5c2a
jinisha <jinisha@incubyte.co>
2022-05-22 11:21:26 +0530

    integration test for getAll words for dictionary controller

--- src/test/java/com/dictionary/DictionaryControllerTest.java
+++ src/test/java/com/dictionary/DictionaryControllerTest.java
@@ -1,14 +1,19 @@
 package com.dictionary;
 
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 import io.micronaut.core.type.Argument;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 @MicronautTest
 class DictionaryControllerTest {
@@ -17,14 +22,37 @@ class DictionaryControllerTest {
   @Client("/")
   HttpClient httpClient;
 
-  @Test
-  void save_new_word() {
+  @BeforeEach
+  void setUp() {
     Dictionary dictionary = new Dictionary();
+    dictionary.setWord("word1");
+    dictionary = httpClient.toBlocking()
+        .retrieve(HttpRequest.POST("/dictionary", dictionary), Argument.of(Dictionary.class));
+    dictionary = new Dictionary();
     dictionary.setWord("word2");
     dictionary = httpClient.toBlocking()
         .retrieve(HttpRequest.POST("/dictionary", dictionary), Argument.of(Dictionary.class));
+  }
 
-    Assertions.assertThat(dictionary.getWord()).isEqualTo("word2");
+  @Test
+  void save_new_word() {
+    Dictionary dictionary = new Dictionary();
+    dictionary.setWord("word");
+    dictionary = httpClient.toBlocking()
+        .retrieve(HttpRequest.POST("/dictionary", dictionary), Argument.of(Dictionary.class));
+
+    Assertions.assertThat(dictionary.getWord()).isEqualTo("word");
+  }
+
+  @Test
+  void get_all_words() {
+
+    List<Dictionary> words = httpClient.toBlocking()
+        .retrieve(HttpRequest.GET("/dictionary/getAllWords"), Argument.listOf(Dictionary.class));
+    List<String> actualWords =
+        words.stream().map(Dictionary::getWord).collect(Collectors.toList());
+    List<String> expectedWords = List.of("word1","word2");
+    assertThat(actualWords).containsAll(expectedWords);
   }
 
 }
